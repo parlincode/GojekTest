1aaff4a6473f6b92783ebcb10cb23ea734d9d05a
Parlincode <parlin.sinabariba@gmail.com>
2017-04-21 21:01:50 +0700

    Penambahan Variable Main

--- app/src/main/java/codetorium/gojektest/MainActivity.java
+++ app/src/main/java/codetorium/gojektest/MainActivity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    Integer main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        main = new Integer(2);
     }
 }
